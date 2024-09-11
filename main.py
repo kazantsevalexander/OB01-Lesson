96c069f2d43c88929c3deeb96d97e47a403f925e
kazantsevalexander <kazantsevalexander@gmail.com>
2024-09-12 00:11:35 +0300

    add new class

---
+++ main.py
@@ -0,0 +1,22 @@
+class Warior():
+    def __init__(self, name, power, endurance, hair_color):
+        self.name = name
+        self.power = power
+        self.endurance = endurance
+        self.hair_color = hair_color
+    def sleep(self):
+        print(f"{self.name} лег спать")
+        self.endurance += 2
+    def eat(self):
+        print(f"{self.name} сел кушать")
+        self.power += 1
+    def hit(self):
+        print(f"{self.name} дерется")
+        self.endurance -= 6
+    def walk(self):
+        print(f"{self.name} гуляет")
+    def info(self):
+        print(f"имя воина - {self.name}")
+        print(f"сила воина - {self.power}")
+        print(f"выносливость воина - {self.endurance}")
+        print(f"цвет волос воина - {self.hair_color}")
